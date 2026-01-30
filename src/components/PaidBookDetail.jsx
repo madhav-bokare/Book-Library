37319cb27fc1abe2fbbfee7e39041da7de952ec2
madhav-bokare <madhavbokare926@gmail.com>
2026-01-30 12:37:07 +0530

    Update API endpoints to use production URL

--- src/components/PaidBookDetail.jsx
+++ src/components/PaidBookDetail.jsx
@@ -16,7 +16,7 @@ const PaidBookDetail = () => {
       try {
         // 🔹 Single book
         const res = await axios.get(
-          `http://localhost:5000/api/book/title/${encodeURIComponent(title)}`
+          `https://book-library-backend-qnfv.onrender.com/api/book/title/${encodeURIComponent(title)}`
         );
 
         if (res.data.link !== "paid") {
@@ -28,7 +28,7 @@ const PaidBookDetail = () => {
 
         // 🔹 Suggestions (same type books)
         const sugRes = await axios.get(
-          `http://localhost:5000/api/book/paid`
+          `https://book-library-backend-qnfv.onrender.com/api/book/paid`
         );
 
         const filtered = sugRes.data.filter(
